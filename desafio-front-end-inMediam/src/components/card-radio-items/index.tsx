8b260697b5790a7b015a2f4a6df20b3124b0e86c
Danny Marques <daninhacmmarques@gmail.com>
2024-07-06 15:27:30 -0300

    feat: criacao componente card-radio-items

---
+++ desafio-front-end-inMediam/src/components/card-radio-items/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { CardProps } from "../../utils/interfaces/card-props";
+const CardRadioItem = ({ dataItem }: CardProps) => {
+  return (
+    <div className="w-full">
+      <div className="w-full flex justify-center items-center cursor-pointer">
+        <div
+          className="w-24 rounded-full h-24 items-center"
+          style={{
+            backgroundImage: `url(${dataItem.imagemCartoon})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+      </div>
+      <div>
+        <p className="text-white text-sm cursor-pointer text-center text-base text-md font-semibold mt-2 hover:text-red-800">
+          {" "}
+          {dataItem.nomeCartoon}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CardRadioItem;
