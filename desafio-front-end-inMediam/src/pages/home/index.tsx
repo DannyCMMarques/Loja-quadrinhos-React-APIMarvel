deeabaeb7298d2a52bf150f984d17f350eb6b86b
Danny Marques <daninhacmmarques@gmail.com>
2024-07-06 14:19:48 -0300

    feat: criacao do componente swiper

--- desafio-front-end-inMediam/src/pages/home/index.tsx
+++ desafio-front-end-inMediam/src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import CardItem from "../../components/cards-item";
 import ContainerItem from "../../components/container";
 import { CardItemProps } from "../../utils/interfaces/card-item-props";
-
+import SwiperComponent from "../../components/swiper";
 const index = () => {
   const item: CardItemProps[] = [
     {
@@ -40,14 +40,23 @@ const index = () => {
         "https://darkside.vtexassets.com/arquivos/ids/195360/causas_nao_naturais_mini_capa.png?v=638519819438170000",
       precoCartoon: 100.5,
     },
+    {
+      id: 5,
+      nomeCartoon: "Homem aranha",
+      imagemCartoon:
+        "https://darkside.vtexassets.com/arquivos/ids/195360/causas_nao_naturais_mini_capa.png?v=638519819438170000",
+      precoCartoon: 100.5,
+    },
   ];
 
   return (
     <ContainerItem>
-      <div className="w-full grid grid-cols-2 sm:grid-cols-2 md:flex gap-4 px-2 mt-32">
-        {item.map((item) => (
-          <CardItem key={item.id} dataItem={item} />
-        ))}
+      <div className="w-full flex grid-cols-2 sm:grid-cols-2 md:flex gap-4 px-2 mt-32">
+        <SwiperComponent quantItemMobile={2} quantItems={5}>
+          {item.map((item) => (
+            <CardItem key={item.id} dataItem={item} />
+          ))}
+        </SwiperComponent>
       </div>
     </ContainerItem>
   );
