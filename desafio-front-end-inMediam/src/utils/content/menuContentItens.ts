61d60e320e9de28baca520c6a48cd087f5f4d665
Danny Marques <daninhacmmarques@gmail.com>
2024-07-05 23:58:48 -0300

    feat: adiciona componente de navegação (navbar)

---
+++ desafio-front-end-inMediam/src/utils/content/menuContentItens.ts
@@ -0,0 +1,10 @@
+import { MenuContentInterface } from "./../interfaces/menuContentInterface";
+
+const menuContentItens: MenuContentInterface[] = [
+  { id: 1, href: "/", title: "Home" },
+  { id: 2, href: "/contatos", title: "Contato" },
+  { id: 3, href: "/herois", title: "Heróis" },
+  { id: 3, href: "/herois", title: "Autores" },
+];
+
+export default menuContentItens;
