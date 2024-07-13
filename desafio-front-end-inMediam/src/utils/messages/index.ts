39e538bd66eb77295e973217f0a2b1c9168dc412
Danny Marques <daninhacmmarques@gmail.com>
2024-07-13 09:07:46 -0300

    update: atualizacao das tipagens e interfaces na pagina carrinho-compra e nos componentes relacionados a essa

--- desafio-front-end-inMediam/src/utils/messages/index.ts
+++ desafio-front-end-inMediam/src/utils/messages/index.ts
@@ -8,13 +8,7 @@ export const MESSAGES = {
   FRASE_4_HOME: "The best comic's series is here",
   FRASE_HOME_DESTAQUE: "Monthly Highlights",
   FRASE_PAGE_INFO: "Maybe you like it",
-  COMPRA_APROVADA: "Yay, purchase approved!",
-  TITULO_PAG_CARRINHO: "My Cart",
-  TOTAL_PAG_CARRINHO: "Total: $",
-  FINALIZAR_COMPRA: "Checkout",
-  NENHUM_ITEM_CARRINHO: "No items in your shopping cart",
-  VA_ATE_SESSAO_COMPRAS: "Go to a section and make your purchases",
-  PAGINA_NAO_ENCONTRADA: "Oops! Page not found",
+
   PAGE_PRODUTO_INFO: {
     ADICIONAR_AO_CARRINHO: "Add to cart",
     PARCELAS: "or 10x of $",
@@ -44,4 +38,15 @@ export const MESSAGES = {
     CEP_NAO_ENCONTRADO: "Zip code not found",
     CEP_FRASE: "Enter the zip code to calculate the price and deadline",
   },
+ CARRINHO_COMPRA:{
+  COMPRA_APROVADA: "Yay, purchase approved!",
+  TITULO_PAG_CARRINHO: "My Cart",
+  TOTAL_PAG_CARRINHO: "Total: $",
+  FINALIZAR_COMPRA: "Checkout",
+  NENHUM_ITEM_CARRINHO: "No items in your shopping cart",
+  VA_ATE_SESSAO_COMPRAS: "Go to a section and make your purchases",
+  PAGINA_NAO_ENCONTRADA: "Oops! Page not found",
+  PRODUTO:"Product",
+  PRECO:"Price",
+ }
 };
